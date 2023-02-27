93afc558c103a068d18e14bdb0ae012ad4452db7
ttse23 <terence.tse23@gmail.com>
2023-02-26 23:13:39 -0800

    Implemented two options to add book. Included book cover art if adding manually

--- main.js
+++ main.js
@@ -9,60 +9,25 @@ class Book {
     }
 }
 
-// class Library {
-//     constructor() {
-//         this.books = []
-//     }
-//     addBookToLibrary(book) {
-//         // if (!this.books.some(b => b.title === book.title)) {
-//         //     this.books.push(book)
-//         // }
-//         this.books.push(book);
-//     }
-//     removeBookFromLibrary(book) {
-//         this.books = this.books.filter(b => b.title !== book.title)
-//     }
-// }
-
-// class UI {
-//     addBook() {
-
-//     }
-//     removeBook() {
-
-//     }
-//     updateBook() {
-
-//     }
-// }
-
-// let myLibrary = new Library();
-// let testbook = new Book('B1', 'Auth1', 100, true)
-// myLibrary.addBookToLibrary(testbook)
-// console.log(myLibrary)
-
-// on page load
-    // if local storage doesn't have item, initialize as empty array
-    // else set library array as JSON.parse('local storage item')
-// let myLibrary = [
-// 	{ title: 'The Hobbit', author: 'J. R. R. Tolkien', pages: 304, read: false },
-//     new Book(`Harry Potter and the Sorcerer's Stone`, 'J. K. Rowling', 336, true),
-//     new Book(`Harry Potter and the Chamber of Secrets`, 'J. K. Rowling', 357, true),
-// ];
-
 function addBookToLibrary(book) {
-    // check if book already exists in library. only proceed if not
-
-    // update local storage
-    const library = JSON.parse(localStorage.getItem('library'));    
-    library.push(book)
-    localStorage.setItem('library', JSON.stringify(library));
+    // check if book already exists in library, only proceed if not
 
     // create book card and add to dom
-    // createBookCard({title, author, pages, read}, myLibrary.length - 1);
+    createBookCard(book);
+
+    // update local storage
+    const library = JSON.parse(localStorage.getItem('library'));
+    library.push(book)
+    localStorage.setItem('library', JSON.stringify(library));    
 }
 
 function deleteBookFromLibrary(book) {
+    // delete book card from dom
+    deleteData.style.opacity = '0';
+    deleteData.addEventListener('transitionend', () => {
+        deleteData.remove()
+    })
+
     // update local storage
     const library = JSON.parse(localStorage.getItem('library'));
     for (let i = 0; i < library.length; i++) {
@@ -72,11 +37,12 @@ function deleteBookFromLibrary(book) {
         }
     }
     localStorage.setItem('library', JSON.stringify(library));
-
-    // delete book card from dom
 }
 
 function changeBookStatus(book) {
+    // modify book card in dom
+        // done thru event listener when creating book card
+
     // update local storage
     const library = JSON.parse(localStorage.getItem('library'));
     for (let i = 0; i < library.length; i++) {
@@ -86,8 +52,6 @@ function changeBookStatus(book) {
         }
     }
     localStorage.setItem('library', JSON.stringify(library));
-
-    // modify book card in dom
 }
 
 function renderLibrary() {
@@ -96,13 +60,9 @@ function renderLibrary() {
     })
 }
 
-function createBookCard(book, index) {
+function createBookCard(book) {
     const bookCard = document.createElement('div');
 
-    // delete animation
-    // bookCard.setAttribute('data-delete', 'zoom');
-
-    // bookCard.setAttribute('data-id', index);
     bookCard.setAttribute('class', 'card shadow h-100 mb-2');
     bookCard.classList.add(book.read ? 'border-left-success' : 'border-left-warning');
 
@@ -115,7 +75,8 @@ function createBookCard(book, index) {
     const bookCardImgDiv = document.createElement('div');
     bookCardImgDiv.setAttribute('class', 'col-md-auto text-center py-2')
     const bookCardImg = document.createElement('img');
-    bookCardImg.src = book.cover;
+    bookCardImg.setAttribute('class', 'book-cover')
+    bookCardImg.src = book.cover ? book.cover :'default.jpg';
     bookCardImgDiv.appendChild(bookCardImg)
     bookCardDetails.appendChild(bookCardImgDiv);
 
@@ -123,10 +84,13 @@ function createBookCard(book, index) {
     bookCardTextDiv.setAttribute('class', 'col py-2')
 
     const bookCardTitle = document.createElement('h3');
+    bookCardTitle.setAttribute('class', 'title')
     bookCardTitle.textContent = book.title;
     const bookCardAuthor = document.createElement('h4');
+    bookCardAuthor.setAttribute('class', 'author')
     bookCardAuthor.textContent = 'by ' + book.author;
     const bookCardPages = document.createElement('h5');
+    bookCardPages.setAttribute('class', 'page-count')
     bookCardPages.textContent = book.pages + ' pages';
 
     bookCardTextDiv.appendChild(bookCardTitle);
@@ -157,32 +121,18 @@ function createBookCard(book, index) {
     bookCardReadBtn.appendChild(readIconSpan)
     bookCardReadBtn.appendChild(readIconSpanText)
 
-    bookCardReadBtn.addEventListener('click', (e) => {
-        if (book.read) {
-            book.read = false;
-            bookCardReadBtn.classList.remove('btn-success');
-            bookCardReadBtn.classList.add('btn-warning');
-            readIcon.setAttribute('class', 'bi bi-bookmark-fill')
-            readIconSpanText.textContent = 'Want to Read';
-            bookCard.classList.remove('border-left-success');
-            bookCard.classList.add('border-left-warning');
-        } else {
-            book.read = true;
-            bookCardReadBtn.classList.remove('class', 'btn-warning');
-            bookCardReadBtn.classList.add('class', 'btn-success');
-            readIcon.setAttribute('class', 'bi bi-check-lg')
-            readIconSpanText.textContent = 'Read';
-            bookCard.classList.remove('border-left-warning');
-            bookCard.classList.add('border-left-success');
-        }
+    bookCardReadBtn.addEventListener('click', () => {
+        bookCardReadBtn.setAttribute('class', book.read ? 'btn btn-icon-split w-100 mb-1 btn-warning' : 'btn btn-icon-split w-100 mb-1 btn-success');
+        readIcon.setAttribute('class', book.read ? 'bi bi-bookmark-fill' : 'bi bi-check-lg');
+        readIconSpanText.textContent = book.read ? 'Want to Read' : 'Read';
+        bookCard.setAttribute('class', book.read ? 'card shadow h-100 mb-2 border-left-warning' : 'card shadow h-100 mb-2 border-left-success');
+        book.read = !book.read;
+
         changeBookStatus(book);
     })
 
     const bookCardDeleteBtn = document.createElement('button');
     bookCardDeleteBtn.setAttribute('class', 'btn btn-danger btn-icon-split w-100');
-
-    bookCardDeleteBtn.setAttribute('data-bs-toggle', 'modal')
-    bookCardDeleteBtn.setAttribute('data-bs-target', '#deleteModal')
 
     const deleteIconSpan = document.createElement('span');
     deleteIconSpan.setAttribute('class', 'icon text-white-50')
@@ -197,31 +147,13 @@ function createBookCard(book, index) {
     bookCardDeleteBtn.appendChild(bookCardDeleteText)
 
     bookCardDeleteBtn.addEventListener('click', (e) => {
-        // user confirm delete?
-        document.getElementById('deleteModalTitle').textContent = `${book.title}`
+        // save book card to global variable
+        deleteData = e.target.closest('.card')
+        deleteBook = book
 
-        document.getElementById('confirmDeleteButtonModal').addEventListener('click', () => {
-            e.target.closest('.card').classList.add('fade-out');
-            
-            // delete from DOM
-            setTimeout(() => {
-                e.target.closest('.card').remove();
-            }, 450);
-
-            // delete book from local storage
-            deleteBookFromLibrary(book);
-        })
-
-        // // fade out effect upon delete
-        // e.target.closest('.card').classList.add('fade-out');
-
-        // // delete from DOM
-        // setTimeout(() => {
-        //     e.target.closest('.card').remove();
-        // }, 450);
-
-        // // delete book from local storage
-        // deleteBookFromLibrary(book);
+        // confirm delete modal
+        document.getElementById('deleteModalTitle').textContent = `${book.title}`;
+        deleteModal.show();
     })
 
     bookCardActions.appendChild(bookCardReadBtn);
@@ -231,49 +163,79 @@ function createBookCard(book, index) {
     bookCardBody.appendChild(bookCardActions);
     bookCard.appendChild(bookCardBody)
 
-    // setTimeout(() => {
-    //     bookCard.classList.add('fade-out');
-    // }, 10)
-
     document.getElementById('book-list').append(bookCard)
 }
+
+let deleteData
+let deleteBook
+const deleteModal = new bootstrap.Modal('#deleteModal')
+
+// MODAL FORM CONTROL //
+const addBookModal = new bootstrap.Modal('#addModal')
+
+document.getElementById('addISBN').addEventListener('click', () => {
+    document.getElementById('manually').classList.add('hidden');
+    document.getElementById('isbn').classList.remove('hidden');
+
+
+    document.querySelectorAll('#manually input').forEach((el) => {
+        el.required = false;
+    });
+    document.querySelectorAll('#isbn input').forEach((el) => {
+        el.required = true;
+    });
+    
+    document.getElementById('addModalTitle').textContent = 'by ISBN'
+    addBookModal.show();
+})
+
+document.getElementById('addManually').addEventListener('click', () => {
+    document.getElementById('isbn').classList.add('hidden');
+    document.getElementById('manually').classList.remove('hidden');
+
+    document.querySelectorAll('#isbn input').forEach((el) => {
+        el.required = false;
+    });
+    document.querySelectorAll('#manually input').forEach((el) => {
+        el.required = true;
+    });
+
+    document.getElementById('addModalTitle').textContent = 'Manually'
+    addBookModal.show();
+})
+
+
+
+document.getElementById('confirmDelete').addEventListener('click', () => {
+    deleteBookFromLibrary(deleteBook);
+})
 
 // render library on page load
 let myLibrary;
 
 if (localStorage.getItem('library') === null || JSON.parse(localStorage.getItem('library')).length === 0) {
-    myLibrary = [
-        // new Book(`The Hobbit`, 'J. R. R. Tolkien', 304, false, 9780044403371),
-        // new Book(`Harry Potter and the Sorcerer's Stone`, 'J. K. Rowling', 336, true),
-        // new Book(`Harry Potter and the Chamber of Secrets`, 'J. K. Rowling', 357, true),
-    ];
-    // for testing purposes, local storage always contains filled array
-    // set to empty array when done testing
+    myLibrary = [];
 } else {
     myLibrary = JSON.parse(localStorage.getItem('library'));
 }
-localStorage.setItem('library', JSON.stringify(myLibrary))
-renderLibrary(myLibrary);
+localStorage.setItem('library', JSON.stringify(myLibrary));
+renderLibrary();
 
 async function getBookDetails(isbn) {
-    // const url = 'https://openlibrary.org/search.json?title=the+lord+of+the+rings&author=tolkein'
-    // let isbn = 9780590353427
+    // let isbn = 9780439708180
     // let isbn = 9780544003415
-    // const url = `https://www.googleapis.com/books/v1/volumes?q=${isbn}`
-    // const url = `https://www.googleapis.com/books/v1/volumes?q=the+lord+of+the+rings`
-    // const url = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`;
-
-    const url = `https://www.googleapis.com/books/v1/volumes?q=${isbn}`;
+    let parsedisbn = isbn.replace(/[^0-9]/g, '');
+    const url = `https://www.googleapis.com/books/v1/volumes?q=isbn:${parsedisbn}`;
     try {
         const response = await fetch(url);
         const data = await response.json();
-        if (!response.ok) {
-            console.log(data.description);
-            return;
-        }
-        return data.items[0].volumeInfo
+        // if (!response.ok) {
+        //     console.log(data.description);
+        //     return;
+        // }
+        return data.items[0].volumeInfo;
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
@@ -286,56 +248,59 @@ async function getBookDetails(isbn) {
 
 
 // Form Submit (add new book)
-const addBook = document.getElementById('new-book-submit')
-addBook.addEventListener('click', async(e) => {
+const submitBook = document.getElementById('new-book-submit')
+submitBook.addEventListener('click', async(e) => {
     // get form data
     const isbn = document.getElementById('isbn-input').value
-    // const title = document.getElementById('title-input').value
-    // const author = document.getElementById('author-input').value
-    // const pages = document.getElementById('pages-input').value
-    // const read = document.getElementById('read-input').checked
+    const title = document.getElementById('title-input').value
+    const author = document.getElementById('author-input').value
+    const pages = document.getElementById('pages-input').value
+    const read = document.getElementById('read-input').checked
 
     if (isbn) {
         e.preventDefault();
-
         const info = await getBookDetails(isbn);
-        // console.log(info)
+        if (!info) {
+            // display error below ISBN input field
+            console.log(`Error - We don't recognize this ISBN. Try entering the ISBN again, or add the book manually. According to our records, a book with the same title already exists in your library!`)
+        } else {
+            const book = new Book(info.title, info.authors[0], info.industryIdentifiers[0].identifier, info.pageCount, read, '');
+            addBookToLibrary(book);
 
-        // create book object
-        // const book = new Book(title, author, Number(pages), read)
+            // clear form
+            clearForm();
 
-        const book = new Book(info.title, info.authors[0], info.industryIdentifiers[0].identifier, info.pageCount, true, info.imageLinks.thumbnail)
-        addBookToLibrary(book)
-        createBookCard(book, myLibrary.length - 1);
-
-        // add book to library array and local storage
-        // addBookToLibrary(book.items[0])
+            // close modal
+            addBookModal.hide();
+        }        
+    } else if (title && author && Number(pages) >= 0) {
+        e.preventDefault();
+        const book = new Book(title, author, '', pages, read, '');
+        addBookToLibrary(book);
 
         // clear form
         clearForm();
 
-        // add book card to DOM
-        // createBookCard({title, author, pages, read}, myLibrary.length - 1);
+        // close modal
+        addBookModal.hide();
     }
 })
 
 function clearForm() {
-    // document.getElementById('title-input').value = '';
-    // document.getElementById('author-input').value = '';
-    // document.getElementById('pages-input').value = '';
-    // document.getElementById('read-input').checked = false;
-
     document.getElementById('isbn-input').value = '';
+    document.getElementById('title-input').value = '';
+    document.getElementById('author-input').value = '';
+    document.getElementById('pages-input').value = '';
+    document.getElementById('read-input').checked = false;
 }
 
-// sort function (newest first (default) and oldest first, title or author A-Z and Z-A, read or not read first)
+// SORT FUNCTION //
 const select = document.getElementById('select-sort')
 select.addEventListener('change', sortLibrary)
 function sortLibrary() {
     const selectedVal = select.value;
     console.log(selectedVal)
     let copy = [...JSON.parse(localStorage.getItem('library'))]
-    console.log(copy)
     if (selectedVal === 'newest') {
         console.table(copy)
     } else if (selectedVal === 'oldest') {
@@ -354,8 +319,16 @@ function sortLibrary() {
         copy = copy.sort((a, b) => b.title.localeCompare(a.title))
         console.table(copy)
     }
+    // renderLibrary();
 }
-// adding new book should also retoggle sort order to default?
+
+// IDEAS:
+// when adding a new book
+    // unshift book obj to local storage array
+    // rerender library
+    // reset sort order to default (newest to oldest)
 // should toggling read/want to read button rerender DOM if sort selection is by book "read" status?
 
 // do not add book if already in library
+// make add book buttons same size
+// edit button within each card that brings up form (filled with data) for editing/saving
